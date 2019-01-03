d704b77f176fb2acbc93366e98826ec729340cd5
fs000 <flaviosantos002010@hotmail.com>
2019-01-03 00:00:49 +0000

    Added a World.cs file that builds a 2D World

---
+++ 2DWorld/World.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DWorld {
+    class World {
+
+        private int x, y;
+
+        public World(int x, int y) {
+
+            this.x = x;
+            this.y = y;
+
+            CreateWorld();
+        }
+
+        private void CreateWorld() {
+
+            for (int i = 0; i < y; i++) {
+
+                for (int j = 0; j < x; j++) {
+
+                    Console.Write("O");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

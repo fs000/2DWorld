31a06721363b9edd56cdfe940cb7aa569d31618b
fs000 <flaviosantos002010@hotmail.com>
2019-01-03 00:22:07 +0000

    Added an Input class that reads the user's input (x and y)

---
+++ 2DWorld/Input.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DWorld {
+    class Input {
+
+        public Input() { }
+
+        public int GetX() {
+
+            Console.Write("X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            return x;
+        }
+
+        public int GetY() {
+
+            Console.Write("Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            return y;
+        }
+    }
+}

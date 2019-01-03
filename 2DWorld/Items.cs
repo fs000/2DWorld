09677f94aac98c8529d63bb0792dcb3bc4d324a8
fs000 <flaviosantos002010@hotmail.com>
2019-01-03 01:23:44 +0000

    Added an Enumeration of Items to be placed on the World

---
+++ 2DWorld/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DWorld {
+
+    [Flags]
+    public enum Items {
+
+        Food = 1,
+        Guns = 2,
+        Enemy = 4,
+        Trap = 8
+    }
+}
